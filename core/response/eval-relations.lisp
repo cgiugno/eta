cb61a2bcef72e9c37ef6334344a8f4d2d5f5b257
bkane2 <Ben.kane@me.com>
2020-01-17 17:03:38 -0500

    Updated historical questions and approximate relations

--- core/response/eval-relations.lisp
+++ core/response/eval-relations.lisp
@@ -6,16 +6,38 @@
 ;; so that the centroid coordinates of two blocks which are touching will differ by 1
 ;;
 
+
+(defun eval-relation (rel coords1 coords2)
+; ``````````````````````````````````````````
+; Evaluate whether relation rel holds between an object with centroid at coords1, and
+; an object with centroid at coords2.
+; NOTE: coords is a list of the form e.g. (|Texaco| 2 3 0)
+;
+  (apply rel (append (cdr coords1) (cdr coords2)))
+) ; END eval-relation
+
+
+
+(defun ~= (x y)
+; ``````````````````````
+; Approximately equal to
+;
+  (let ((eps 0.1))
+    (< (abs (- x y)) eps))
+) ; END ~=
+
+
+
 (defun touching.p (x1 y1 z1 x2 y2 z2)
 ; ````````````````````````````````````
   (let ((diffx (abs (- x1 x2))) (diffy (abs (- y1 y2))) (diffz (abs (- z1 z2))))
     (or
-      (and (= 0 diffx) (> 1 diffy) (= 1 diffz))
-      (and (> 1 diffx) (= 0 diffy) (= 1 diffz))
-      (and (= 0 diffx) (= 1 diffy) (> 1 diffz))
-      (and (> 1 diffx) (= 1 diffy) (= 0 diffz))
-      (and (= 1 diffx) (= 0 diffy) (> 1 diffz))
-      (and (= 1 diffx) (> 1 diffy) (= 0 diffz))))
+      (and (~= 0 diffx) (>  1 diffy) (~= 1 diffz))
+      (and (>  1 diffx) (~= 0 diffy) (~= 1 diffz))
+      (and (~= 0 diffx) (~= 1 diffy) (>  1 diffz))
+      (and (>  1 diffx) (~= 1 diffy) (~= 0 diffz))
+      (and (~= 1 diffx) (~= 0 diffy) (>  1 diffz))
+      (and (~= 1 diffx) (>  1 diffy) (~= 0 diffz))))
 ) ; END touching.p
 
 
