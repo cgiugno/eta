a2665733baf1e83ccf9cd34950c1826a8d4ebfab
bkane2 <Ben.kane@me.com>
2020-02-10 23:03:02 -0500

    Simplified semantics/gist-clauses/topic-keys in schema

--- avatars/sophie/schemas/eta-schema.lisp
+++ avatars/sophie/schemas/eta-schema.lisp
@@ -25,56 +25,48 @@
 
 ?a3. (Me react-to.v ?a2.) ; React to comment, or initiate sub-schema to respond to follow-up question
 
-)); end of defparameter *eta-schema*
+)) ; END defparameter *eta-schema*
 
 
 
-
+;````````````````````````````````````````````````````````
+; Create empty hash tables for semantics,
+; gist-clauses, and topic-keys
+;
 (setf (get '*eta-schema* 'semantics) (make-hash-table))
+(setf (get '*eta-schema* 'gist-clauses) (make-hash-table))
+(setf (get '*eta-schema* 'topic-keys) (make-hash-table))
+
+
+
 ;````````````````````````````````````````````````````````
 ; EL Semantics - Not yet used
 ;
-(defun store-output-semantics (var wff schema-name)
-  (setf (gethash var (get schema-name 'semantics)) wff)
-); end of store-output-semantics
-
 (mapcar #'(lambda (x)
       (store-output-semantics (first x) (second x) '*eta-schema*))
   '()
-); end of mapcar #'store-output-semantics
+) ; END mapcar #'store-output-semantics
 
 
 
-
-(setf (get '*eta-schema* 'gist-clauses) (make-hash-table))
 ;````````````````````````````````````````````````````````
 ; Gist clauses
 ;
-(defun store-output-gist-clauses (var clauses schema-name)
-  (setf (gethash var (get schema-name 'gist-clauses)) clauses)
-); end of store-output-gist-clauses
-
 (mapcar #'(lambda (x) 
       (store-output-gist-clauses (first x) (second x) '*eta-schema*))
   '(
     (?a1.  ((what do you want to talk about ?)))
   )
-); end of mapcar #'store-output-gist-clauses
+) ; END mapcar #'store-output-gist-clauses
 
 
 
-
-(setf (get '*eta-schema* 'topic-keys) (make-hash-table))
 ;````````````````````````````````````````````````````````
 ; Topic keys
 ;
-(defun store-topic-keys (var keys schema-name)
-  (setf (gethash var (get schema-name 'topic-keys)) keys)
-); end of store-topic-keys
-
 (mapcar #'(lambda (x) 
       (store-topic-keys (first x) (second x) '*eta-schema*))
   '(
     (?a1.  (topic))
   )
-); end of mapcar
+) ; END mapcar #'store-topic-keys
