ee32d283f3eaedad813e42f1831de68e6cba7dff
bkane2 <Ben.kane@me.com>
2020-02-17 17:44:07 -0500

    Added to chemo topic

--- avatars/sophie/rules/subtopics/rules-for-chemotherapy-details-input.lisp
+++ avatars/sophie/rules/subtopics/rules-for-chemotherapy-details-input.lisp
@@ -5,6 +5,11 @@
 
 (READRULES '*chemotherapy-details-input*
 '(
+  ; Many different types of chemotherapy
+  
+  ; Doublet chemotherapy
+
+  ; Nausea, vomiting, hair loss, low blood counts, infections
   1 (0)
     2 *general-input* (0 :subtree)
 ))
