f0c89b03f13458c89fe5608de952373f0e3808ec
bkane2 <Ben.kane@me.com>
2020-07-27 15:16:18 -0400

    Modified day6 rule files

--- avatars/lissa-elderly/day6/rules/choose-gist-clause-trees-for-input.lisp
+++ avatars/lissa-elderly/day6/rules/choose-gist-clause-trees-for-input.lisp
@@ -29,22 +29,29 @@
 ;;   This is why we don't simply use a verbatim match to Lissa's
 ;;   output.
 
-(eval-when (load eval)
-
 (READRULES '*gist-clause-trees-for-input*
- '(1 (2 What changes 1 happen 1 you 2 next few years 2)
-    2 (*specific-answer-from-life-changes-next-years-input* 
-       *unbidden-answer-from-life-changes-next-years-input*
-       *thematic-answer-from-life-changes-next-years-input*
-       *question-from-life-changes-next-years-input*) (0 :subtrees)
+'(
+   ; household-chores
+   1 (3 what 3 household chores 4 today 3)
+      2 (*householdChores-today-input*) (0 :subtrees)
+   1 (3 what 2 household chore 2 enjoy 3)
+      2 (*householdChore-enjoy-input*) (0 :subtrees)
+   1 (3 How 4 feel 3 helped 4 household chore 3)
+      2 (*householdChore-help-others-input*) (0 :subtrees)
+
+   ; money
+   1 (2 What 2 ways 1 manage 1 money 2)
+      2 (*manage-money-ways-input*) (0 :subtrees)
+   1 (2 Is managing money 2 stressful 5)
+      2 (*manage-money-stressful-input*) (0 :subtrees)
+   1 (2 How 2 learn 2 managing 2 money 2)
+      2 (*manage-money-learn-input*) (0 :subtrees)
+
+   ; growing-older
+   1 (2 What changes 1 happen 1 you 2 next few years 2)
+      2 (*life-changes-next-years-input*) (0 :subtrees)
    1 (2 What 2 hardest part 2 growing older 2)
-    2 (*specific-answer-from-growing-older-hardest-part-input* 
-       *unbidden-answer-from-growing-older-hardest-part-input*
-       *thematic-answer-from-growing-older-hardest-part-input*
-       *question-from-growing-older-hardest-part-input*) (0 :subtrees)
+      2 (*growing-older-hardest-part-input*) (0 :subtrees)
    1 (2 What 2 best part 2 growing older 2)
-    2 (*specific-answer-from-growing-older-best-part-input* 
-       *unbidden-answer-from-growing-older-best-part-input*
-       *thematic-answer-from-growing-older-best-part-input*
-       *question-from-growing-older-best-part-input*) (0 :subtrees)
-)))
+      2 (*growing-older-best-part-input**) (0 :subtrees)
+))
--- avatars/lissa-elderly/day6/rules/choose-reaction-to-input.lisp
+++ avatars/lissa-elderly/day6/rules/choose-reaction-to-input.lisp
@@ -25,56 +25,67 @@
 ;;  What is the hardest part of growing older ?
 ;;  What is the best part of growing older ? 
 
-(eval-when (load eval)
-
-(mapc 'attachfeat ; needed for detecting alternatives in the
+(MAPC 'ATTACHFEAT ; needed for detecting alternatives in the
                   ; watching-or-reading question
-  '( ))
+'(
+))
+
 
 (READRULES '*reaction-to-input*
-  ; Choose between reaction to a question and an assertion
-  ; Only one gist clause is expected here
- '(1 (0 wh_ 3 you 0)
+; Choose between reaction to a question and an assertion
+; Only one gist clause is expected here
+'(
+  1 (0 wh_ 3 you 0)
     2 *reaction-to-question* (0 :subtree)
-   1 (0 wh_ 3 your 0)
+  1 (0 wh_ 3 your 0)
     2 *reaction-to-question* (0 :subtree)
-   1 (0 aux your 0)
+  1 (0 aux your 0)
     2 *reaction-to-question* (0 :subtree)
-   1 (0 aux you 0)
+  1 (0 aux you 0)
     2 *reaction-to-question* (0 :subtree)
-   1 (0 right-really 4 ?)
+  1 (0 right-really 4 ?)
     2 *reaction-to-question* (0 :subtree)
-   1 (0); by default, it's an assertion
+  1 (0); by default, it's an assertion
     2 *reaction-to-assertion* (0 :subtree)
- ))
+))
+
 
 (READRULES '*reaction-to-assertion*
-  ; Very rough initial attempt.
-  ; Actually, it seems we could readily provide reactions
-  ; directly here, instead of delegating to specialized
-  ; choice trees. However, it seems we have better oversight
-  ; by using separate choice trees, specified in a file that
-  ; also contains the specialized features for the topic at
-  ; issue.
-  ;
-; e.g., 
- '(   1 (0 change 2 happen 3 next few years 0)
+; Very rough initial attempt.
+; Actually, it seems we could readily provide reactions
+; directly here, instead of delegating to specialized
+; choice trees. However, it seems we have better oversight
+; by using separate choice trees, specified in a file that
+; also contains the specialized features for the topic at
+; issue.
+'(
+  ; household-chores
+  1 (0 household chore 4 today 0)
+    2 *reaction-to-householdChores-today-input* (0 :subtree)
+  1 (0 household chore 4 enjoy 0) 
+    2 *reaction-to-householdChore-enjoy-input* (0 :subtree) 
+  1 (0 I helped someone with 0) 
+    2 *reaction-to-householdChore-help-others-input* (0 :subtree)
+  1 (0 felt 2 I helped someone with household chores 0)
+    2 *reaction-to-householdChore-help-others-input* (0 :subtree)
+
+  ; money
+  1 (0 way 3 manage 1 money 0)
+    2 *reaction-to-manage-money-ways-input* (0 :subtree) 
+  1 (0 managing money is not stressful 0)
+    2 *reaction-to-manage-money-stressful-input* (0 :subtree)
+  1 (0 managing money is stressful 0)
+    2 *reaction-to-manage-money-stressful-input* (0 :subtree)
+  1 (0 learned 2 managing money 0) 
+    2 *reaction-to-manage-money-learn-input* (0 :subtree)
+
+  ; growing-older
+  1 (0 change 2 happen 3 next few years 0)
     2 *reaction-to-life-changes-next-years-input* (0 :subtree)
-    
-; e.g., 
-   1 (0 hardest part 1 growing older 0)
-    2 *reaction-to-growing-older-hardest-part-input* (0 :subtree)
-
-	; e.g., 
-   1 (0 best part 1 growing older 0)
+  1 (0 hardest part 1 growing older 0)
+    2 *reaction-to-growing-older-hardest-part-input* (0 :subtree) 
+  1 (0 best part 1 growing older 0)
     2 *reaction-to-growing-older-best-part-input* (0 :subtree)
-
-   ))
+))
  
- (READRULES '*reaction-to-unexpected*
-   '(1 (0 thank you 0)
-      2 *user-thanks-schema* (0 :schema))
- )
- 
-)
 
