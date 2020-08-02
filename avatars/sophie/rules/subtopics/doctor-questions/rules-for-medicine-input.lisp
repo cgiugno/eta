62b37455afdb93d2bb5dc5543ac0cfc4f3006be9
cgiugno <67064875+cgiugno@users.noreply.github.com>
2020-08-02 15:46:59 -0400

    Update rules-for-medicine-input.lisp

--- avatars/sophie/rules/subtopics/doctor-questions/rules-for-medicine-input.lisp
+++ avatars/sophie/rules/subtopics/doctor-questions/rules-for-medicine-input.lisp
@@ -1,78 +1,110 @@
 (MAPC 'ATTACHFEAT
-'(
-  (have having)
-  (trouble troubles problem problems)
-  (concentrate concentrating focus focusing)
-  (ment-health anxiety anxious depression depressed health illness)
-  (work working help helping treat treating effective)
-  (med-take take taking get getting use using)
-  (med-give recommend recommending recommended recommend advise advising advises advised suggests suggesting suggested suggest gives give given giving prescribe prescribes prescribed prescribed)
-  (antidepressant SSRI Prozac fluoxetine sertraline Zoloft Celexa citalopram Lexapro escitalopram paroxetine Paxil Pexeva)
-  (therapy therapist counseling counselor)
-  (med-narcotic narcotic narcotics oxycodone morphine)
+  '(
+    (med-take take taking get getting use using)
+    (med-past took taken used gotten prescribed)
+    (work working help helping treat treating effective)
+    (med-narcotic narcotic narcotics oxycodone morphine)
+    (often frequent frequently much)
+    (med-time every time times hour hours minute minutes day days week weeks)
+    (want need)
+    (med-better better stronger effective heavier)
+    (med-for for work)
+    (med-help care help helping helpful better effective work working)
   ))
 
 
-(READRULES '*energy-input*
+(READRULES '*medicine-input*
 '(
-    1 (0 be-aux 3 ment-health 3 recently 0)
-    2 ((how is your mental health ?) (energy)) (0 :gist)
-  1 (0 how 5 think-gen 0)
-    2 ((how is your mental health ?) (energy)) (0 :gist)
-  1 (0 how 3 ment-health 5)
-    2 ((how is your mental health ?) (energy)) (0 :gist)
-  1 (0 wh_ 5 think-gen 0)
-    2 ((how is your mental health ?) (energy)) (0 :gist)
-  1 (0 do 1 pron 5 ment-health 0 )
-    2 ((how is your mental health ?) (energy)) (0 :gist)
-  1 (0 have 1 pron 8 ment-health 0)
-    2 ((how is your mental health ?) (energy)) (0 :gist)
 
-  1 (0 antidepressant 0)
-    2 ((You should take an antidepressant \.) (medicine-request)) (0 :gist)
-  1 (0 therapy 0)
-    2 ((You should see a therapist \.) (energy)) (0 :gist)
+  ;; ; The following needs modification
+  ;; 1 (0 sure \, I can arrange that 0)
+  ;;   2 ((I will give you more medicine \.) (stronger-medicine)) (0 :gist)
 
+  ; Possibly too general, might need refining
   1 (0 med-narcotic 0)
-    2 *medicine-input* (0 :subtree)
- 
- 1 (0)
-    2 *general-input* (0 :subtree)
-))
+    2 ((You should take a narcotic \.) (medicine-request)) (0 :gist)
 
-
-(READRULES '*energy-question*
-'(
-  1 (0 be-aux 5 ment-health 5)
-    2 ((How is your mental health ?) (energy)) (0 :gist)
-  1 (0 trouble 1 concentrate 0)
-    2 ((Have you had trouble concentrating ?) (energy)) (0 :gist)
-  1 (0 you 1 have 1 energy 0)
-    2 ((How is your energy ?) (energy)) (0 :gist)
-  1 (0 trouble 1 energy 0)
-    2 ((How is your energy ?) (energy)) (0 :gist)
-  1 (0 med-take 5 antidepressant)
-    2 ((You should take an antidepressant \.) (medicine-request)) (0 :gist)
-  1 (0 therapy 0)
-    2 ((You should see a therapist \.) (energy)) (0 :gist)
   1 (0)
-    2 ((NIL Gist)) (0 :gist)
-
+    2 *general-input* (0 :subtree)
+  1 (0)
+    2 ((NIL Gist \: nothing found for medicine \.)) (0 :gist)
 ))
 
 
-(READRULES '*energy-reaction*
+(READRULES '*medicine-question*
 '(
-  1 (I can give you an antidepressant \.)
-    2 (*have-subdialogue* ((I see\. Would this help me with my pain ?)
-                           ((Will an antidepressant help with my pain ?)))) (100 :schema+args)
-    2 (Okay\. You\'ll give the antidepressant medication a shot\.) (0 :out)
-  1 (You should take an antidepressant \.)
-    2 (*have-subdialogue* ((I see\. Would this help me with my pain ?)
-                           ((Will an antidepressant help with my pain ?)))) (100 :schema+args)
-    2 (Okay\. You\'ll give the antidepressant medication a shot\.) (0 :out)
-  1 (You should see a therapist \.)
-    2 (*have-subdialogue* ((I\'m not sure\. Do you think there\'s any medication I can try first ?)
-                           ((Should I try medication before I try therapy ?)))) (100 :schema+args)
-    2 (Okay\. You\'ll give the therapy a shot\.) (0 :out)
+  ; Do you have allergies?
+  1 (0 have 0 allergies 0)
+    2 ((Do you have allergies to any medicine ?) (medicine-allergies)) (0 :gist)
+
+  ; How often are you taking medication?
+  1 (0 how 1 often 3 med-take 0)
+    2 ((How often are you taking medication ?) (medicine-frequency)) (0 :gist)
+  1 (0 be 0 med-take 2 often 0)
+    2 ((How often are you taking medication ?) (medicine-frequency)) (0 :gist)
+  1 (0 be 0 med-take 2 med-time 0)
+    2 ((How often are you taking medication ?) (medicine-frequency)) (0 :gist)
+
+  ; Does taking the medicine more frequently help?
+  1 (0 medicine-taking 3 frequently 5 med-help 0)
+    2 ((Does taking medication more frequently help ?) (medicine-frequency)) (0 :gist)
+  1 (0 med-help 5 medicine-taking 3 frequently 0)
+    2 ((Does taking medication more frequently help ?) (medicine-frequency)) (0 :gist)
+  1 (0 medicine-taking 5 med-help 5 frequently 0)
+    2 ((Does taking medication more frequently help ?) (medicine-frequency)) (0 :gist)
+
+  ; Are you taking [some med]?
+  1 (0 be 2 you 8 med-take 0 medicine-particular 0)
+    2 ((Are you taking 8 ?) (medicine)) (0 :gist)
+  1 (0 what medicine 3 you 2 med-take 0)
+    2 ((What medicine are you taking ?) (medicine)) (0 :gist)
+
+  ; Is the pain medication working?
+  1 (0 be-aux 3 medicine-taking 3 med-help 3 pain 0)
+    2 ((Is the pain medication working at all ?) (medicine-working)) (0 :gist)
+  1 (0 be-aux 0 medicine-taking 3 do anything 0)
+    2 ((Is the pain medication working at all ?) (medicine-working)) (0 :gist)
+  1 (0 be-aux 0 medicine-taking 0 med-help 3 at all 0)
+    2 ((Is the pain medication working at all ?) (medicine-working)) (0 :gist)
+  1 (0 be-aux 0 medicine-taking 0 med-help 0)
+    2 ((Is the pain medication working ?) (medicine-working)) (0 :gist)
+  1 (0 how 0 medicine-taking 0 med-help 0)
+    2 ((Is the pain medication working ?) (medicine-working)) (0 :gist)
+
+  ; Do you want stronger pain medication?
+  1 (0 do 1 you 3 want 3 med-better 1 medicine-taking 0)
+    2 ((Do you want stronger pain medication ?) (medicine-request)) (0 :gist)
+  1 (0 do 1 you 3 want 3 med-narcotic 0)
+    2 ((Do you want stronger pain medication ?) (medicine-request)) (0 :gist)
+  1 (0 wh_ 5 think-gen 5 med-take 1 med-better 1 medicine-taking 0)
+    2 ((Do you want stronger pain medication ?) (medicine-request)) (0 :gist)
+  1 (0 wh_ 5 think-gen 5 med-take 1 med-narcotic 0)
+    2 ((Do you want stronger pain medication ?) (medicine-request)) (0 :gist)
+  1 (0 how 3 you 5 think-gen 5 med-take 1 med-better 1 medicine-taking 0)
+    2 ((Do you want stronger pain medication ?) (medicine-request)) (0 :gist)
+  1 (0 how 3 you 5 think-gen 5 med-take 1 med-narcotic)
+    2 ((Do you want stronger pain medication ?) (medicine-request)) (0 :gist)
+  
+  ; Do you need more medicine?
+  1 (0 do 1 you 3 want 2 medicine-taking 0)
+    2 ((Do you need more medicine ?) (medicine-refill)) (0 :gist)
+  1 (0 how are you 1 medicine-taking 0); e.g., how are you on medicine?
+    2 ((Do you need more medicine ?) (medicine-refill)) (0 :gist)
+
+  1 (0 you 3 med-take 5 med-narcotic 0)
+    2 ((You should take a narcotic \.) (medicine-request)) (0 :gist)
+
+  ; Do you have a history with narcotics?
+  1 (0 be-aux 3 med-past 3 med-narcotic 0)
+    2 ((What is your history with med-narcotic ?) (medical-history)) (0 :gist)
+  1 (0 be-aux 3 history 3 med-narcotic 0)
+    2 ((What is your history with med-narcotic ?) (medical-history)) (0 :gist)
+
+  ))
+
+
+(READRULES '*medicine-reaction*
+'(
+  ;; 1 (0 I will give you more medicine 0)
+  ;;   2 (Great\, thank me !) (0 :out)
 ))
