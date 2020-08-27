1717fa503f7d9a0817cde38100aef3cbcac2582c
cgiugno <67064875+cgiugno@users.noreply.github.com>
2020-08-27 09:20:29 -0400

    Update rules-for-test-results-input.lisp

--- avatars/sophie/rules/subtopics/sophie-questions/rules-for-test-results-input.lisp
+++ avatars/sophie/rules/subtopics/sophie-questions/rules-for-test-results-input.lisp
@@ -3,6 +3,10 @@
   (body-part lung lungs bone bones chest)
   (cure curable recover recovery solution fix miracle)
   (cancer-increase increase spread)
+  (metastatic metastasized metastasize)
+  (scan CT cat)
+  (image picture x-ray view)
+  (takes records took taken recorded produces produced provides provided makes made)
 ))
 
 
@@ -29,12 +33,24 @@
     2 ((The test results show that your cancer has spread \.) (test-results)) (0 :gist)
   1 (0 tumor 2 in 6 chest 0)
     2 ((The test results show that your cancer has spread \.) (test-results)) (0 :gist)
+  1 (0 metastatic 0)
+    2 ((The test results show that your cancer has spread \.) (test-results)) (0 :gist)
 
   ; You have stage 4 cancer
   1 (0 stage four 0)
     2 ((The test results show that your cancer has spread \.) (test-results)) (0 :gist)
   1 (0 advanced 1 body-part 1 cancer 0)
     2 ((The test results show that your cancer has spread \.) (test-results)) (0 :gist)
+
+  ; A CT scan is...
+  1 (0 scan 2 be-aux 0)
+    2 ((A CT scan is like a more accurate X ray \.) (test-results)) (0 :gist)
+  1 (0 scan 3 takes 3 image 0)
+    2 ((A CT scan is like a more accurate X ray \.) (test-results)) (0 :gist)
+  1 (0 scan 3 image 3 takes 0)
+    2 ((A CT scan is like a more accurate X ray \.) (test-results)) (0 :gist)
+  1 (0 scan 5 inside 3 body 0)
+    2 ((A CT scan is like a more accurate X ray \.) (test-results)) (0 :gist)
 
   1 (0)
     2 *general-input* (0 :subtree)
