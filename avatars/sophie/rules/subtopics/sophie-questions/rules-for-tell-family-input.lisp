c2ac0a4d618d7983a0c7d105bb403707ea6769d8
cgiugno <67064875+cgiugno@users.noreply.github.com>
2020-08-27 09:22:25 -0400

    Update rules-for-tell-family-input.lisp

--- avatars/sophie/rules/subtopics/sophie-questions/rules-for-tell-family-input.lisp
+++ avatars/sophie/rules/subtopics/sophie-questions/rules-for-tell-family-input.lisp
@@ -1,10 +1,33 @@
 (MAPC 'ATTACHFEAT
 '(
+  (family relative relatives children daughter son child grandchildren sibling siblings)
+  (told mentioned explained said)
+  (know knows idea realize knowledge aware informed inform)
+  (cancer condition illness sickness)
+  (anyone any someone person)
+  (conversation talk discussion appointment)
+  (want like desire)
 ))
 
 
 (READRULES '*tell-family-input*
 '(
+  ; Does your family know that you have cancer ?
+  1 (0 does 4 family 4 know 0)
+    2 ((Does your family know that you have cancer ?) (tell-family)) (0 :gist)
+  1 (0 be-aux 4 family 4 know 0)
+    2 ((Does your family know that you have cancer ?) (tell-family)) (0 :gist)
+  1 (0 be-aux 3 cancer 3 told 5 family 0)
+    2 ((Does your family know that you have cancer ?) (tell-family)) (0 :gist)
+
+  ; Is there anyone else you want here for this conversation?
+  1 (0 anyone 5 here 4 want 5 conversation 0)
+    2 ((Is there someone you want here for this conversation ?) (tell-family)) (0 :gist)
+  1 (0 want 3 anyone 7 conversation 0)
+    2 ((Is there someone you want here for this conversation ?) (tell-family)) (0 :gist)
+  1 (0 anyone 5 want 4 here 5 conversation 0)
+    2 ((Is there someone you want here for this conversation ?) (tell-family)) (0 :gist)
+
   1 (0)
     2 *general-input* (0 :subtree)
   1 (0)
@@ -14,6 +37,21 @@
 
 (READRULES '*tell-family-question*
 '(
+  ; Does your family know that you have cancer ?
+  1 (0 does 4 family 4 know 0)
+    2 ((Does your family know that you have cancer ?) (tell-family)) (0 :gist)
+  1 (0 be-aux 4 family 4 know 0)
+    2 ((Does your family know that you have cancer ?) (tell-family)) (0 :gist)
+  1 (0 be-aux 3 cancer 3 told 5 family 0)
+    2 ((Does your family know that you have cancer ?) (tell-family)) (0 :gist)
+
+ ; Is there anyone else you want here for this conversation?
+  1 (0 anyone 5 here 4 want 5 conversation 0)
+    2 ((Is there someone you want here for this conversation ?) (tell-family)) (0 :gist)
+  1 (0 want 3 anyone 7 conversation 0)
+    2 ((Is there someone you want here for this conversation ?) (tell-family)) (0 :gist)
+  1 (0 anyone 5 want 4 here 5 conversation 0)
+    2 ((Is there someone you want here for this conversation ?) (tell-family)) (0 :gist)
 
 ))
 
